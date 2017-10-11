0ddd829bd6ae5286f53c45257f726051654e29c1
Sujay Patwardhan <ptwrdhn2@illinois.edu>
2017-10-11 15:06:41 +0000

    mvp

--- Assignment2.0/src/driver.rb
+++ Assignment2.0/src/driver.rb
@@ -2,9 +2,15 @@ require './scraper'
 class Driver
   def initialize
     scraper = Scraper.new('https://en.wikipedia.org/wiki/Top_Gun')
+
+    puts scraper.graph.movies_year(1986).length
+    puts scraper.graph.movie_actors("Top Gun")
+    puts scraper.graph.movie_actors("Tom Cruise")
+    puts scraper.graph.gross("Top Gun")
+
     scraper.dump_json
     scraper.read_json
-    printf("asdfasdfasdf: ")
+
   end
 
 end
