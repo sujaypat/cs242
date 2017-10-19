cf981661e1ae97ecf68a38753ca22973cc40b15f
Sujay Patwardhan <ptwrdhn2@illinois.edu>
2017-10-19 20:39:33 +0000

    init commit

---
+++ Assignment3.0/GitHub Viewer/GitHub Viewer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GitHub Viewer
+//
+//  Created by Sujay Patwardhan on 10/19/17.
+//  Copyright © 2017 Sujay Patwardhan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
