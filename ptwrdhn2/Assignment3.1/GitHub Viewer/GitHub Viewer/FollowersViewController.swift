70d7d83ad98a829fcd3830a452bb6e56de4263b4
ptwrdhn2 <ptwrdhn2@c830209f-e47d-4feb-a7dd-e718440f945f>
2017-10-24 03:26:19 +0000

    tiuh3rhu

---
+++ ptwrdhn2/Assignment3.1/GitHub Viewer/GitHub Viewer/FollowersViewController.swift
@@ -0,0 +1,84 @@
+//
+//  SecondViewController.swift
+//  GitHub Viewer
+//
+//  Created by Sujay Patwardhan on 10/19/17.
+//  Copyright © 2017 Sujay Patwardhan. All rights reserved.
+//
+
+import UIKit
+import Alamofire
+import SwiftyJSON
+import SafariServices
+
+class FollowersViewController: UITableViewController {
+
+    struct Person {
+        let userName : String
+        let url : URL
+        let img : URL
+    }
+    var people = [Person]()
+    var json : JSON = JSON.null;
+    var user : String = USER + "/followers";
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        getUserData(url: user)
+    }
+    
+    func getUserData(url: String) {
+        Alamofire.request(url, method: .get).validate().responseJSON { response in
+            switch response.result {
+            case .success(let value):
+                self.json = JSON(value)
+//                print("JSON: \(self.json)")
+                self.parseJSON()
+            case .failure(let error):
+                print(error)
+            }
+        }
+    }
+    
+    func parseJSON(){
+        let repoArray = self.json.arrayValue
+        for repo in repoArray {
+            let name = repo["login"].stringValue
+            let url = URL(string: repo["html_url"].stringValue)
+            let imgURL = URL(string: repo["avatar_url"].stringValue)
+            let cell = Person(userName: name, url: url!, img: imgURL!)
+            people.append(cell)
+        }
+        
+        self.tableView.reloadData()
+    }
+    
+    override func tableView(_: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return people.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        let person = people[indexPath.row]
+        cell.imageView!.image = UIImage(data: try! Data(contentsOf: person.img))
+        cell.textLabel!.text = person.userName
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: false)
+//        let view = SFSafariViewController(url: people[indexPath.row].url)
+//        self.present(view, animated: true, completion: nil)
+        performSegue(withIdentifier: "showProfileView", sender: indexPath)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let profileViewController = segue.destination as? ProfileViewController,
+            let indexPath = sender as? IndexPath {
+            profileViewController.username = self.people[indexPath.row].userName
+        }
+    }
+
+}
+
---
+++ ptwrdhn2/Assignment3.1/GitHub Viewer/GitHub Viewer/ProfileViewController.swift
@@ -0,0 +1,103 @@
+//
+//  FirstViewController.swift
+//  GitHub Viewer
+//
+//  Created by Sujay Patwardhan on 10/19/17.
+//  Copyright © 2017 Sujay Patwardhan. All rights reserved.
+//
+
+import UIKit
+import Foundation
+import Alamofire
+import SwiftyJSON
+
+let USER : String = "https://api.github.com/users/sujaypat";
+
+class ProfileViewController: UIViewController {
+    
+    var json : JSON = JSON.null;
+    @IBOutlet weak var avatar: UIImageView!
+    @IBOutlet weak var actualName: UILabel!
+    @IBOutlet weak var userName: UILabel!
+    @IBOutlet weak var bio: UILabel!
+    @IBOutlet weak var website: UILabel!
+    @IBOutlet weak var email: UILabel!
+    @IBOutlet weak var repos: UIButton!
+    @IBOutlet weak var followers: UIButton!
+    @IBOutlet weak var following: UIButton!
+    @IBOutlet weak var created: UILabel!
+    
+    var username = "sujaypat"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        getUserData(url: "https://api.github.com/users/\(self.username)")
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func getUserData(url: String) {
+        Alamofire.request(url, method: .get).validate().responseJSON { response in
+            switch response.result {
+            case .success(let value):
+                self.json = JSON(value)
+//                print("JSON: \(self.json)")
+                self.downloadImage(url: URL(string: self.json["avatar_url"].stringValue)!)
+                self.actualName.text = self.json["name"].stringValue
+                self.userName.text = self.json["login"].stringValue
+                self.bio.text = self.json["bio"].string ?? "No bio provided :("
+                self.website.text = self.json["blog"].string ?? "No website provided :("
+                self.email.text = self.json["email"].string ?? "No public email :("
+                self.created.text = "created on: " + self.json["created_at"].stringValue
+                self.repos.setTitle((self.json["public_repos"].stringValue + " public repos"), for: .normal)
+                self.followers.setTitle(("Followers: " + self.json["followers"].stringValue), for: .normal)
+                self.following.setTitle(("Following: " + self.json["following"].stringValue), for: .normal)
+            case .failure(let error):
+                print(error)
+            }
+        }
+    }
+    
+    @IBAction func changeTab(sender: UIButton) {
+        let label = sender.currentTitle!
+        print(label)
+        if label.contains("repos"){
+            super.tabBarController!.selectedIndex = 1;
+        }
+        if label.contains("Following"){
+            super.tabBarController!.selectedIndex = 2;
+        }
+            if label.contains("Followers"){
+            super.tabBarController!.selectedIndex = 3;
+        }
+    }
+    
+    
+    // The below functions are from a stack overflow question about downloading images from a url
+    // to use in an ImageView
+    func getDataFromUrl(url: URL, completion: @escaping (Data?, URLResponse?, Error?) -> ()) {
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            completion(data, response, error)
+            }.resume()
+    }
+    
+    func downloadImage(url: URL) {
+        print("Download Started")
+        getDataFromUrl(url: url) { data, response, error in
+            guard let data = data, error == nil else { return }
+            print(response?.suggestedFilename ?? url.lastPathComponent)
+            print("Download Finished")
+            DispatchQueue.main.async() {
+                self.avatar.image = UIImage(data: data)
+            }
+        }
+    }
+
+
+}
+
