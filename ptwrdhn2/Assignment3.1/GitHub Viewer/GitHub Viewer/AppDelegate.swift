63e8e1a5fd454dc475d0fae69f751dfa820f506e
ptwrdhn2 <ptwrdhn2@c830209f-e47d-4feb-a7dd-e718440f945f>
2017-11-01 04:06:39 +0000

    done

--- ptwrdhn2/Assignment3.1/GitHub Viewer/GitHub Viewer/AppDelegate.swift
+++ ptwrdhn2/Assignment3.1/GitHub Viewer/GitHub Viewer/AppDelegate.swift
@@ -14,10 +14,10 @@ import OAuthSwift
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-
-
+    var oauthswift: OAuth2Swift?
+    
     func application(_ app: UIApplication, open url: URL, options: [UIApplicationOpenURLOptionsKey : Any] = [:]) -> Bool {
-        if (url.host == "github-viewer") {
+        if (url.host == "asdf") {
             OAuthSwift.handle(url: url)
         }
         return true
@@ -31,6 +31,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationDidEnterBackground(_ application: UIApplication) {
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+        CoreDataManager.shared.saveContext()
     }
 
     func applicationWillEnterForeground(_ application: UIApplication) {
@@ -42,78 +43,146 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
     
     func applicationDidFinishLaunching(_ application: UIApplication) {
-        let oauthswift = OAuth2Swift(
+        let _ = CoreDataManager.shared
+        
+        oauthswift = OAuth2Swift(
             consumerKey:    "cc8b40bc12358262d119",
-            consumerSecret: "794c5f03030c713b896ba9a7302af2d95889b50e",
+            consumerSecret: "2f8cc0d76aee2fed36e88293263c1c1e19021ba2",
             authorizeUrl:   "https://github.com/login/oauth/authorize",
+            accessTokenUrl: "https://github.com/login/oauth/access_token",
             responseType:   "token"
         )
-        let handle = oauthswift.authorize(
+        let _ = oauthswift!.authorize(
             withCallbackURL: "github-viewer://asdf",
-            scope: "user",
+            scope: "user,public_repo",
             state: "authorized",
             success: { credential, response, parameters in
+                UserModel.shared.key = credential.oauthToken
                 print(credential.oauthToken)
-                // Do your request
             },
             failure: { error in
-                print(error.localizedDescription)
+                print(error, error.localizedDescription)
             }
         )
+        
+        
+        GithubService.getFollowers(byName: USER)
+        .onSuccess{ (json) in
+            guard let json = json as? [[String: Any]] else { return }
+            
+            DispatchQueue.main.async {
+                
+                for item in json {
+                    guard let id = item["id"] as? Int64 else { continue }
+                    
+                    let fetchRequest = NSFetchRequest<Person>(entityName: "Person")
+                    fetchRequest.predicate = NSPredicate(format: "id == \(id)")
+                    guard let result = try? CoreDataManager.shared.persistentContainer.viewContext.fetch(fetchRequest) else { continue }
+
+                    var person: Person
+                    switch result.count {
+                    case 0:
+                        person = Person(context: CoreDataManager.shared.persistentContainer.viewContext)
+                        person.following = false
+                    case 1:
+                        person = result[0]
+                    default:
+                        fatalError("Internal error")
+                    }
+                    
+                    person.id       = id
+                    person.follower = true
+                    person.imgURL   = item["avatar_url"] as? String ?? ""
+                    person.url      = item["html_url"]   as? String ?? ""
+                    person.userName = item["login"]      as? String ?? ""
+                }
+            }
+        }
+        .onFailure { (reason) in
+            print(reason)
+        }
+        .perform(withAuthorization: nil)
+        
+        
+        GithubService.getFollowing(byName: USER)
+        .onSuccess{ (json) in
+            guard let json = json as? [[String: Any]] else { return }
+            
+            DispatchQueue.main.async {
+                
+                for item in json {
+                    guard let id = item["id"] as? Int64 else { continue }
+                    
+                    let fetchRequest = NSFetchRequest<Person>(entityName: "Person")
+                    fetchRequest.predicate = NSPredicate(format: "id == \(id)")
+                    guard let result = try? CoreDataManager.shared.persistentContainer.viewContext.fetch(fetchRequest) else { continue }
+                    
+                    var person: Person
+                    switch result.count {
+                    case 0:
+                        person = Person(context: CoreDataManager.shared.persistentContainer.viewContext)
+                        person.follower = false
+                    case 1:
+                        person = result[0]
+                    default:
+                        fatalError("Internal error")
+                    }
+                    
+                    person.id        = id
+                    person.following = true
+                    person.imgURL    = item["avatar_url"] as? String ?? ""
+                    person.url       = item["html_url"]   as? String ?? ""
+                    person.userName  = item["login"]      as? String ?? ""
+                }
+            }
+        }
+        .onFailure { (reason) in
+            print(reason)
+        }
+        .perform(withAuthorization: nil)
+        
+        GithubService.getRepos(byName: USER)
+        .onSuccess { (json) in
+            guard let json = json as? [[String: Any]] else { return }
+            
+            DispatchQueue.main.async() {
+                
+                for item in json {
+                    guard let name = item["name"] as? String else { continue }
+                    
+                    let fetchRequest = NSFetchRequest<Repo>(entityName: "Repo")
+                    fetchRequest.predicate = NSPredicate(format: "name == %@", name)
+                    guard let result = try? CoreDataManager.shared.persistentContainer.viewContext.fetch(fetchRequest) else { continue }
+                    
+                    var repo: Repo
+                    switch result.count {
+                    case 0:
+                        repo = Repo(context: CoreDataManager.shared.persistentContainer.viewContext)
+                    case 1:
+                        repo = result[0]
+                    default:
+                        fatalError("Internal error")
+                    }
+                    repo.name     = name
+                    let owner     = item["owner"]       as? [String: Any]
+                    repo.userName = owner?["login"]     as? String ?? ""
+                    repo.desc     = item["description"] as? String ?? "No description provided"
+                    repo.url      = item["html_url"]    as? String ?? ""
+                
+                }
+            }
+        }
+        .onFailure { (reason) in
+            print(reason)
+        }
+        .perform(withAuthorization: nil)
+
     }
     
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
         // Saves changes in the application's managed object context before the application terminates.
-        self.saveContext()
+        CoreDataManager.shared.saveContext()
     }
-    
-    // MARK: - Core Data stack
-    
-    lazy var persistentContainer: NSPersistentContainer = {
-        /*
-         The persistent container for the application. This implementation
-         creates and returns a container, having loaded the store for the
-         application to it. This property is optional since there are legitimate
-         error conditions that could cause the creation of the store to fail.
-         */
-        let container = NSPersistentContainer(name: "GitHub Viewer")
-        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
-            if let error = error as NSError? {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                
-                /*
-                 Typical reasons for an error here include:
-                 * The parent directory does not exist, cannot be created, or disallows writing.
-                 * The persistent store is not accessible, due to permissions or data protection when the device is locked.
-                 * The device is out of space.
-                 * The store could not be migrated to the current model version.
-                 Check the error message to determine what the actual problem was.
-                 */
-                fatalError("Unresolved error \(error), \(error.userInfo)")
-            }
-        })
-        return container
-    }()
-    
-    // MARK: - Core Data Saving support
-    
-    func saveContext () {
-        let context = persistentContainer.viewContext
-        if context.hasChanges {
-            do {
-                try context.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nserror = error as NSError
-                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
-            }
-        }
-    }
-
-
-
 }
 
--- ptwrdhn2/Assignment3.1/GitHub Viewer/GitHub ViewerUITests/GitHub_ViewerUITests.swift
+++ ptwrdhn2/Assignment3.1/GitHub Viewer/GitHub ViewerUITests/GitHub_ViewerUITests.swift
@@ -9,7 +9,7 @@
 import XCTest
 
 class GitHub_ViewerUITests: XCTestCase {
-        
+    
     override func setUp() {
         super.setUp()
         
@@ -17,8 +17,6 @@ class GitHub_ViewerUITests: XCTestCase {
         
         // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
-        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
-        XCUIApplication().launch()
 
         // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
     }
@@ -28,9 +26,77 @@ class GitHub_ViewerUITests: XCTestCase {
         super.tearDown()
     }
     
-    func testExample() {
+    func testVisitAllTabs() {
+        XCUIDevice.shared().orientation = .portrait
+        let app = XCUIApplication()
+        app.launch()
+        sleep(10)
+        app.activate() // Go back to app
+        sleep(5)
+        let tabBarsQuery = app.tabBars
+        tabBarsQuery.buttons["Repos"].tap()
+        tabBarsQuery.buttons["Following"].tap()
+        tabBarsQuery.buttons["Followers"].tap()
+    }
+    
+    func testStarRepo() {
         // Use recording to get started writing UI tests.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
+        
+        XCUIDevice.shared().orientation = .portrait
+        let app = XCUIApplication()
+        app.launch()
+        sleep(10)
+        app.activate() // Go back to app
+        sleep(5)
+        let tabBarsQuery = app.tabBars
+        tabBarsQuery.buttons["Repos"].tap()
+        
+        let tablesQuery = XCUIApplication().tables
+        tablesQuery.cells["seabuffalo, A C (sea) port of Buffalo."].swipeLeft()
+        tablesQuery/*@START_MENU_TOKEN@*/.buttons["Star/Unstar"]/*[[".cells[\"seabuffalo, A C (sea) port of Buffalo.\"].buttons[\"Star\/Unstar\"]",".buttons[\"Star\/Unstar\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
     }
+    
+    func testUnfollowUser(){
+        // Use recording to get started writing UI tests.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        
+        XCUIDevice.shared().orientation = .portrait
+        let app = XCUIApplication()
+        app.launch()
+        sleep(10)
+        app.activate() // Go back to app
+        sleep(5)
+        
+        app.tabBars.buttons["Following"].tap()
+        let tablesQuery = XCUIApplication().tables
+        tablesQuery/*@START_MENU_TOKEN@*/.staticTexts["theishshah"]/*[[".cells[\"theishshah\"].staticTexts[\"theishshah\"]",".staticTexts[\"theishshah\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.swipeLeft()
+        sleep(1)
+        tablesQuery/*@START_MENU_TOKEN@*/.cells["theishshah"].buttons["Unfollow"]/*[[".cells[\"theishshah\"].buttons[\"Unfollow\"]",".buttons[\"Unfollow\"]"],[[[-1,1],[-1,0]]],[1]]@END_MENU_TOKEN@*/.tap()
+        sleep(5)
+        XCTAssert(app.tables.cells.count == 9)
+    }
+    
+    func testFollowUser(){
+        // Use recording to get started writing UI tests.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        
+        XCUIDevice.shared().orientation = .portrait
+        let app = XCUIApplication()
+        app.launch()
+        sleep(10)
+        app.activate() // Go back to app
+        sleep(5)
+        
+        app.tabBars.buttons["Following"].tap()
+        app.navigationBars["Following"].buttons["Add"].tap()
+        
+        app.alerts["Follow"].typeText("theishshah")
+        app.alerts["Follow"].buttons["OK"].tap()
+        sleep(5)
+        XCTAssert(app.tables.cells.count == 9)
+    }
+    
+    
     
 }
